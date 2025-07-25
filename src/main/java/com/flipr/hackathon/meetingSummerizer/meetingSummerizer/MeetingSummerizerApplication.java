d116b4940ad19c3c983ad4a947e09288d140f2c1
Spidermanmil <sonijayesh281103@gmail.com>
2025-07-25 19:29:17 +0530

    Fresh start: Add Spring Boot meetingSummerizer project

---
+++ src/main/java/com/flipr/hackathon/meetingSummerizer/meetingSummerizer/MeetingSummerizerApplication.java
@@ -0,0 +1,13 @@
+package com.flipr.hackathon.meetingSummerizer.meetingSummerizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingSummerizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingSummerizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flipr/hackathon/meetingSummerizer/meetingSummerizer/MeetingSummerizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flipr.hackathon.meetingSummerizer.meetingSummerizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingSummerizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
